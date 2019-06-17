a27829b0bbe74d8c0d911e26490770abd5674ed2
Harsh Khirwal <harshkhirwal24@gmail.com>
2019-06-18 00:59:21 +0530

    Hashing password

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
@@ -11,25 +10,33 @@ var multer = require('multer');
 const Subject = require('../models/subject');
 var Question = require('../mongo-models/index');
 var passport = require('passport');
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
 var session = require('express-session');
+var bcrypt = require('bcrypt');
 
-var crypto            = require('crypto');
-var LocalStrategy     = require('passport-local').Strategy;
-var Store             = require('express-session').Store;
+var crypto = require('crypto');
+var LocalStrategy = require('passport-local').Strategy;
+var Store = require('express-session').Store;
 var BetterMemoryStore = require('session-memory-store')(session);
 
-var store = new BetterMemoryStore({ expires: 60 * 60 * 1000, debug: true });
- router.use(session({
-    name: 'JSESSION',
-    secret: 'nitrkl',
-    store:  store,
-    resave: true,
-    saveUninitialized: true
+var store = new BetterMemoryStore({
+  expires: 60 * 60 * 1000,
+  debug: true
+});
+router.use(session({
+  name: 'JSESSION',
+  secret: 'nitrkl',
+  store: store,
+  resave: true,
+  saveUninitialized: true
 }));
 
 
-router.use(session({ secret: 'nitrkl' ,saveUninitialized: false, resave: false})); // session secret
+router.use(session({
+  secret: 'nitrkl',
+  saveUninitialized: false,
+  resave: false
+})); // session secret
 router.use(passport.initialize());
 router.use(passport.session()); // persistent login sessions
 require('../../config/passport')(passport);
@@ -39,20 +46,24 @@ module.exports = (app) => {
   app.use('/', router);
 };
 
-router.get('/login', function(req, res){
-  if(req.query.register != "success")
+router.get('/login', function(req, res) {
+  if (req.query.register != "success")
     res.render('loginPage');
   else
-    res.render('loginPage', { successMessage: "You have registered successfully" });
+    res.render('loginPage', {
+      successMessage: "You have registered successfully"
+    });
 });
 
 
 router.post("/login", passport.authenticate('login', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/loginfailed',
-    failureFlash: true
-}), function(req, res, info){
-    res.render('loginPage',{'message' : req.flash('message')});
+  successRedirect: '/dashboard',
+  failureRedirect: '/loginfailed',
+  failureFlash: true
+}), function(req, res, info) {
+  res.render('loginPage', {
+    'message': req.flash('message')
+  });
 });
 
 function isAuthenticated(req, res, next) {
@@ -61,55 +72,71 @@ function isAuthenticated(req, res, next) {
   res.redirect('/login');
 }
 
-router.get('/loginfailed', function(req, res){
-	res.render('loginPage',{ errorMessage: req.flash('loginMessage') });
+router.get('/loginfailed', function(req, res) {
+  res.render('loginPage', {
+    errorMessage: req.flash('loginMessage')
+  });
 });
 
-router.get('/dashboard', function(req, res){
+router.get('/dashboard', function(req, res) {
   console.log(req.user)
-	res.render('dashboard', {layout : "homeLayout" , name: req.user.name });
+  res.render('dashboard', {
+    layout: "homeLayout",
+    name: req.user.name
+  });
 });
 
 router.get('/logout', function(req, res) {
-	req.logout();
-	res.redirect('/login');
+  req.logout();
+  res.redirect('/login');
 });
 
-router.get("/register", function(req, res){
+router.get("/register", function(req, res) {
   res.render("registerPage");
 
 });
-router.post("/register", function(req, res){
 
-email = req.body.email;
-var sqlQuery = "SELECT * FROM Users WHERE email = ? LIMIT 1";
-con.query(sqlQuery, req.body.email, function(error, results){
-  // There was an issue with the query
-  if(error){
-    console.log(error);
-    return;
-  }
-
-  if(results.length){
-    // The username already exists
-    console.log("Id exists");
-    res.redirect('/registerfailed');
-
-  }else{
-    // The username wasn't found in the database
-    console.log("ID doesnt exist");
-    db.User.create({
-      name:  req.body.name,
-      email: req.body.email,
-      password: req.body.password
+router.post("/register", function(req, res) {
+  var password = req.body.password;
+  db.User.findAll({
+    where: {
+      email: req.body.email
+    }
+  }).then(function(results) {
+    if (results.length) {
+      console.log("ID exists");
+      res.redirect('/registerfailed');
+    } else {
+      // The username wasn't found in the database
+      console.log("ID doesnt exist");
+      bcrypt.hash(password, 10, function(err, hash) {
+        if (err) console.log(err);
+        password = hash;
+        db.User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: password
         });
-        res.redirect('/login?register=success');
-      }
-    });
+        // console.log(password);
+        return password;
+
+        //>>query logic should go here.
+      });
+
+      res.redirect('/login?register=success');
+    }
+  }).catch(function(err) {
+    res.status(400).json({
+      error: err
+    })
+    return;
+  });
 });
 
-router.get('/registerfailed', function(req, res){
-	res.render('registerPage',{ errorMessage: 'User already exists' });
+router.get('/registerfailed', function(req, res) {
+  res.render('registerPage', {
+    errorMessage: 'User already exists'
+  });
 });
 
 function checkloginstate(req, res, next) {
--- app/models/user.js
+++ app/models/user.js
@@ -1,17 +1,16 @@
-
 module.exports = (sequelize, DataTypes) => {
 
   const User = sequelize.define('User', {
-  userId: {
-       type: DataTypes.INTEGER ,
-       primaryKey: true,
+    userId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
     },
-  name: DataTypes.STRING,
-  email: DataTypes.STRING,
-  password: DataTypes.STRING
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
   }, {
     timestamps: true
-}, {
+  }, {
     classMethods: {
       associate: (models) => {
         // example on how to add relations
